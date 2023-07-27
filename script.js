19dd0fb20fdde31c8ca675b5fde0a64edf833626
Thielison Alves <67246254+thielison@users.noreply.github.com>
2023-07-27 17:12:19 -0300

    Add game board module with dropToken and printGameBoard functions

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 "use strict";
 
 const boardDiv = document.querySelector(".board-container");
+
+// Create game board
+const gameBoard = (function () {
+    const board = ["", "", "", "", "", "", "", "", ""];
+
+    const getBoard = () => board;
+
+    const dropToken = (column, player) => {
+        if (board[column] !== "") {
+            return;
+        }
+        board[column] = player;
+    };
+
+    const printGameBoard = () => {
+        board.forEach((item, index) => {
+            const divCell = document.createElement("div");
+            divCell.textContent = `${item}`;
+            divCell.classList.add("cell");
+            divCell.dataset.column = `${index}`; // Create a data attribute to identify the column on the board
+            boardDiv.append(divCell);
+        });
+    };
+
+    printGameBoard();
+
+    return { getBoard, dropToken, printGameBoard };
+})();
