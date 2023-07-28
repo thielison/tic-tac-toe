0442764dac470a056927e5027ec4f19fc6500ec9
Thielison Alves <67246254+thielison@users.noreply.github.com>
2023-07-27 21:15:50 -0300

    Add textContent that shows the player's turn on the screen

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const gameBoard = (function () {
 
 // Controls the flow and state of the game's turns,
 // as well as whether anybody has won the game
-const gameController = ((playerOneName = "Player One", playerTwoName = "Player Two") => {
+const gameController = ((playerOneName = "Player X", playerTwoName = "Player O") => {
     const players = [
         { name: playerOneName, token: "X" },
         { name: playerTwoName, token: "O" },
@@ -41,21 +41,27 @@ const gameController = ((playerOneName = "Player One", playerTwoName = "Player T
 
     const getActivePlayer = () => activePlayer;
 
-    // PRINT NEW ROUND ("PLAYER X TURN) VAI AQUI
-
     const switchPlayerTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
     };
+
+    /*  This is where we would check for a winner and handle that logic,
+        such as a win message. */
+    // Create win conditions in the console version, and create a way to check if a win condition was met after each round.
+    // acho que vai ter que pegar o board e verificar todo ele com o forEach a cada token adicionado.
+    // verificar se houve win se preencher algum dos winning combos ou se é um tie com todos preenchidos...
+    // lembrando que com todos preenchidos, na última preenchida, pode haver um jogador vencedor
 
     return { getActivePlayer, switchPlayerTurn };
 })();
 
 const screenController = (function () {
     const board = gameBoard.getBoard();
+    const playerTurn = document.querySelector(".turn");
+    playerTurn.textContent = `${gameController.getActivePlayer().name}'s turn...`;
 
     function clickHandlerBoard(e) {
         const activePlayer = gameController.getActivePlayer();
-
         const selectedColumn = e.target.dataset.column;
 
         // Make sure I've clicked a cell and not the gaps in between
@@ -64,12 +70,15 @@ const screenController = (function () {
         // Prevents switching players if I click on a cell that already have a mark
         if (board[selectedColumn] !== "") return;
 
+        // Update player's turn on each click
+        playerTurn.textContent = `${activePlayer.name}'s turn...`;
+
+        // Drop a player's X/O token on the board
         gameBoard.dropToken(selectedColumn, activePlayer.token);
 
+        // Clear the board container and print the newest version of the board
         const updateScreen = () => {
-            // Clear the board container
             boardDiv.textContent = "";
-            // Get the updated array containing the newest version of the board
             gameBoard.printGameBoard();
         };
 
